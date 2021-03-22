4b59d610acffe35ef37b36468e84e97269a3dde3
DriesSey <dries.seynaeve@kcl.ac.uk>
2021-03-22 07:50:33 +0100

    Added my init.el files so I can look at these from another pc.

---
+++ init.el
@@ -0,0 +1,39 @@
+
+
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+
+(package-initialize)
+
+;; Bootstrap 'use-package'
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/myinit.org"))
+
+
+
+
+
+
+
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(magit modern-cpp\.font-lock yasnippet-classic-snippets jedi flycheck elpy ox-reveal htmlize zenburn-theme which-key vterm use-package try origami org-bullets moe-theme counsel color-theme auto-complete ace-window)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
