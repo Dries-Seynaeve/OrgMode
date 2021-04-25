79cf5e549c00e6f68326c32ee5e4b50fdc6dd050
Dries-Seynaeve <dries.seynaeve@kuleuven.be>
2021-04-25 15:05:32 +0200

    Added the init.el file in order to fetch to most recent org mode.

--- init.el
+++ init.el
@@ -5,6 +5,8 @@
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
 	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "https://prgmode.org/elpa/"))
 
 
 (package-initialize)
@@ -30,7 +32,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(magit modern-cpp\.font-lock yasnippet-classic-snippets jedi flycheck elpy ox-reveal htmlize zenburn-theme which-key vterm use-package try origami org-bullets moe-theme counsel color-theme auto-complete ace-window)))
+   '(visual-fill-column ag fira-code-mode firecode-theme undo-tree counsel-projectile projectile rainbow-delimiters doom-themes virtualenv ivy-rich all-the-icons doom-modeline vterm-toggle magit modern-cpp\.font-lock yasnippet-classic-snippets jedi flycheck elpy ox-reveal htmlize zenburn-theme which-key vterm use-package try origami org-bullets moe-theme counsel color-theme auto-complete ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
